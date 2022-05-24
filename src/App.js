0c8af63a98b8c19f77b5671fbe168e2aaa93b5a7
Kirill Privalenkov <privalenkovkirill@gmail.com>
2022-05-25 01:44:54 +0300

    added collapsed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,53 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import JsonEditor from './components/jsonEditor/jsonEditor';
-import jsonFile from './payload/data.json';
+// import json from './payload/data.json';
 
 function App() {
+  const [json, setJson] = useState([{
+    "name": "a",
+    "email": null,
+    "password": 123,
+    "password2": true,
+    "errors": {
+        "name": "",
+        "email": [
+            "email", 
+            {
+                "message": "email",
+                "path": "email",
+                "type": "email",
+                "context": {
+                    "value": "email",
+                    "key": "email"
+                },
+                "name": "email",
+                "rule": "email",
+                "scope": "email",
+                "messages": ["email"],
+                "code": "EMAIL",
+                "isJoi": true
+            }
+        ],
+        "password": "",
+        "password2": ""
+    }
+    
+}]);
   const jsonEditorHandler = (jsonFile) => {
 
   };
+
+  useState(() => {
+    setJson(json);
+  }, [json])
+
+
   return (
     <div className="App">
       <div className='container'>
-        <JsonEditor onSave={jsonEditorHandler} jsonFile={jsonFile} />
+    
+        <JsonEditor onSave={jsonEditorHandler} jsonFile={json} style={{'borderRadius': '35px'}}/>
       </div>
     </div>
   );  
--- src/components/jsonEditor/jsonEditor.js
+++ src/components/jsonEditor/jsonEditor.js
@@ -2,11 +2,10 @@ import React, { useState, useEffect, useRef } from 'react';
 import ContextMenu from '../contextMenu/contextMenu';
 import './jsonEditor.css';
 
-export default function JsonEditor({jsonFile}) {
+export default function JsonEditor({jsonFile, onSave, style}) {
     const [editorTree, setEditorTree] = useState([]);
     const ref = useRef();
-    
-    const data = jsonFile;
+    const [data, setData] = useState(jsonFile);
 
     const type = (value) => {
         const regex = /^\[object (\S+?)\]$/;
@@ -21,7 +20,7 @@ export default function JsonEditor({jsonFile}) {
             case 'array':
                 return currentValue.length;
             default:
-                return null;
+                return 0;
         }
     }
 
@@ -33,7 +32,7 @@ export default function JsonEditor({jsonFile}) {
 
     const jsonIterator = (data, cb) => {
         let depth = 0;
-        let depthCurrentKey = [];
+        let depthCurrentKey = {};
         (function iter (data, cb) {
             for (let key in data) {
                 const object = {
@@ -41,17 +40,17 @@ export default function JsonEditor({jsonFile}) {
                     value: String(data[key]),
                     type: type(data[key]),
                     paramsInside: insideCounter(data[key], type(data[key])),
-                    parent: depthCurrentKey.length ? depthCurrentKey : [],
+                    parent: depthCurrentKey,
                     depth,
                 };
                 cb.apply(this, [object]);  
                 if (data[key] !== null && typeof(data[key]) === 'object') {
-                    depthCurrentKey = [key, type(data[key])];
+                    depthCurrentKey = {key, type: type(data[key])};
                     depth++;
                     iter(data[key], cb);
                 }
             }
-            depthCurrentKey = [];
+            depthCurrentKey = {};
             depth--;
         })(data, cb)
     }
@@ -70,7 +69,7 @@ export default function JsonEditor({jsonFile}) {
             case 'null':
                 return 'json-editor-tree__row--type-string';
             default:
-                return null;
+                return '';
         }
     }
 
@@ -81,28 +80,47 @@ export default function JsonEditor({jsonFile}) {
     }, []);
 
     const onClickDropdownHandler = (e) => {
-        console.log(e)
+        const current = ref.current;
+        const expandablesValues = current.querySelectorAll('.json-editor__json-editor-value');
+        console.log(e.target.parentNode);
+        expandablesValues.forEach(elValue => {
+            const parentAttr = e.target.parentNode.getAttribute('data-expandable-name');
+            const valueAttr = elValue.getAttribute('data-expadable-parent');
+            if(parentAttr === valueAttr) {
+                elValue.parentNode.parentNode.classList.toggle('collapsed');
+            }
+        });
     }
 
     return (
         <>
             <ContextMenu elRef={ref} options={[{text: 'True'}, {text: 'False'}]}/>
-            <div id="json-editor" ref={ref}>
+            <div id="json-editor" ref={ref} style={{...style}}>
                 <header className='json-editor__header'>React-json-editor</header>
+                {data && <table>
+                    <tbody>
+                        <tr>
+                            <td className='json-editor__line-num'></td>
+                            <td className='json-editor__first-type-title-arrow'>▼</td>
+                            <td className='json-editor__first-type-title'>{type(data)}</td>
+                        </tr>
+                    </tbody>
+                </table>}
                 <div className="json-editor__json-editor-tree">
                     <table>
                         <tbody>
                             {editorTree && editorTree.map((obj, index) => {
-                            return <tr key={index} className={getClassByType(obj.type)}>
+                                console.log(obj)
+                            return <tr key={index} data-expandable-name={obj.key} className={`json-editor__row ${getClassByType(obj.type)} ${obj.paramsInside ? 'json-editor__row--expandable' : ''}`}>
                                     <td className='json-editor__line-num'>{index + 1}</td>
-                                    <td onClick={onClickDropdownHandler} className='json-editor__dropdown-arrow'>{obj.paramsInside && '▼'}</td>
+                                    <td onClick={onClickDropdownHandler} className='json-editor__dropdown-arrow'>{obj.paramsInside ? '▼' : ''}</td>
                                     <td>
-                                        <table className='json-editor__json-editor-value'>
+                                        <table data-expadable-parent={obj.parent.key} className='json-editor__json-editor-value'>
                                             <tbody>
                                                 <tr>
                                                     {depthElCreator(obj.depth)}
-                                                    <td><div className={`json-editor__field-key ${obj.parent[1] === 'array' && 'json-editor__field-key--array'}`}>{obj.key}</div></td>
-                                                    <td className='json-editor__separator'>{!obj.paramsInside && ':'}</td>
+                                                    <td><div className={`json-editor__field-key ${obj.parent.type === 'array' ? 'json-editor__field-key--array' : ''}`}>{obj.key}</div></td>
+                                                    <td className='json-editor__separator'>{!obj.paramsInside ? ':' : ''}</td>
                                                     {
                                                         (obj.type === 'object' || obj.type === 'array')
                                                             ? <td><span className='json-editor__field-params-count-separator'>{ obj.type === 'object' ? '{' : '['}</span><span className='json-editor__field-params-count'>{obj.paramsInside}</span><span className='json-editor__field-params-count-separator'>{ obj.type === 'object' ? '}' : ']'}</span></td>
